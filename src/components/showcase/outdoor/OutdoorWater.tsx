c868a269bbb6ecd730d1ee484ab811ab90803885
༺TᕼᕮᖇᕮᗩᒪᘔᗩYᑎᕮ༻ <therealzayne@yahoo.com>
2026-04-06 08:14:56 -0400

    Create OutdoorWater.tsx

---
+++ src/components/showcase/outdoor/OutdoorWater.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const OutdoorWater: React.FC = () => {
+    return (
+        <div className="site-page-content">
+            <h1>Water & Thrills</h1>
+            <h3>Kayaking and Adrenaline</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    - <strong>Fishing:</strong> One of the most relaxing things I do, especially from my kayak. Casting a line is the perfect way to slow down.<br /><br />
+                    - <strong>Cliff Jumping:</strong> For the adrenaline side, jumping into lakes or rivers is the perfect mix of adventure and excitement.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default OutdoorWater;

31d01be065bb3849e4fb009987fd8e61b21ac609
༺TᕼᕮᖇᕮᗩᒪᘔᗩYᑎᕮ༻ <therealzayne@yahoo.com>
2026-04-06 08:14:34 -0400

    Create OutdoorLand.tsx

---
+++ src/components/showcase/outdoor/OutdoorLand.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const OutdoorLand: React.FC = () => {
+    return (
+        <div className="site-page-content">
+            <h1>Land & Trail</h1>
+            <h3>Endurance and Exploration</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    - <strong>Hiking:</strong> I enjoy lightweight overnight hiking on the <strong>Appalachian Trail</strong>, chasing scenic views and star-filled skies.<br /><br />
+                    - <strong>Biking:</strong> Whether hitting local trails or cruising town, it's my favorite way to clear my head.<br /><br />
+                    - <strong>Camping:</strong> Pitching a tent and sitting around a campfire is the best way to recharge.<br /><br />
+                    - <strong>Track and Field:</strong> This year I’m pushing my endurance and speed to the next level by trying out for the team!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default OutdoorLand;

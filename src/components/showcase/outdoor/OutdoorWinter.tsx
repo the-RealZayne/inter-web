d0d8264a0df07658d66f275a05270fbf80415717
༺TᕼᕮᖇᕮᗩᒪᘔᗩYᑎᕮ༻ <therealzayne@yahoo.com>
2026-04-06 08:15:21 -0400

    Create OutdoorWinter.tsx

---
+++ src/components/showcase/outdoor/OutdoorWinter.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const OutdoorWinter: React.FC = () => {
+    return (
+        <div className="site-page-content">
+            <h1>Winter Sports</h1>
+            <h3>Mountains and Highlights</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    - <strong>Skiing:</strong> I’ve been skiing for almost 10 years at mountains like <strong>Sunday River</strong> and <strong>Sugarloaf</strong>. Check my YouTube for GoPro highlights!<br /><br />
+                    - <strong>Snowboarding (Coming Soon):</strong> This upcoming season I’m starting my journey on a board. Expect progress updates and plenty of wipeouts!
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default OutdoorWinter;
